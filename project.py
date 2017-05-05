9cc8c64d4a1ea8373ad3717e549ed88142898037
jananiperumal <Janani@Jananis-MacBook-Pro.local>
2017-05-05 03:08:33 +0200

    third commit

--- project.py
+++ project.py
@@ -1,28 +1,50 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 import sys
+import numpy
 from test import Ui_Dialog
 from test1window import Ui_test1window
-
-class testwindow(Ui_test1window):
-    def __init__(self, dialog):
-        Ui_test1window.__init__(self)
-        self.setAttribute(QtCore.Qt.WA_DeleteOnClose)
-        self.setupUi(dialog)
-        #testing
+from project1 import testwindow
 
 class FirstGuiProgram(Ui_Dialog):
     def __init__(self, dialog):
         Ui_Dialog.__init__(self)
         self.setupUi(dialog)
+
+        #Logic for clicking the push button and getting new window
         self.pushButton.clicked.connect(self.myfunction)
 
+        #Loading a image in the grapical view widget
+        img_sinogram = QtGui.QImage('/Users/Janani/Downloads/phantom.png')
+        img_sinogram = img_sinogram.scaled(231, 191 , aspectRatioMode=QtCore.Qt.KeepAspectRatio,transformMode=QtCore.Qt.SmoothTransformation)
+        self.pix_sinogram = QtGui.QPixmap(img_sinogram)
+        self.gps_sinogram_placeholder = QtWidgets.QGraphicsPixmapItem(self.pix_sinogram)
+        #self.offset = numpy.zeros(shape=(3, 2))
+        #self.offset[0, ...] = [-self.pix_sinogram.width() / 2, -self.pix_sinogram.height() * 3 / 4]
+        self.gps_sinogram = QtWidgets.QGraphicsPixmapItem(self.pix_sinogram)
+        #self.gps_sinogram.setOffset(self.offset[0, 0], self.offset[0, 1])
+        #self.gps_sinogram.setPos(-  self.offset[0, 0], -self.offset[0, 1])
+
+        self.gs_sinogram = QtWidgets.QGraphicsScene()
+
+        self.gs_sinogram.addItem(self.gps_sinogram)
+
+        self.GV_phantom.setScene(self.gs_sinogram)
+
+        #slide bar with image movement
+        #self.hSlider_1.valueChanged.connect(self.hslider_changed)
+
     def myfunction(self):
-        window = testwindow(self)
-        window.show()
+        self.project1_widget = QtWidgets.QWidget()
+        self.project1_creator = testwindow(self.project1_widget)
+        self.project1_widget.show()
+
+    #def hslider_changed(self):
+
+
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
-    dialog = QtWidgets.QDialog()
+    dialog = QtWidgets.QMainWindow()
     prog = FirstGuiProgram(dialog)
     dialog.show()
     sys.exit(app.exec_())
