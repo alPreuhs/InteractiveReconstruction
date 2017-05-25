5188bd784cd9d1744a21379ccffb7d4280f60692
jananiperumal <Janani@Jananis-MacBook-Pro.local>
2017-05-25 06:21:31 +0200

    change commit

--- fangui_main.py
+++ fangui_main.py
@@ -39,29 +39,41 @@ class fanbeam_main(Ui_wid_FanRecont):
         img_Phantom = img_Phantom.scaled(377,217, aspectRatioMode=QtCore.Qt.KeepAspectRatio,transformMode=QtCore.Qt.SmoothTransformation)
         self.pix_Phantom = QtGui.QPixmap(img_Phantom)
         self.gps_Phantom_placeholder = QtWidgets.QGraphicsPixmapItem(self.pix_Phantom)
-        self.offset = np.zeros(shape=(3, 2))
-        self.offset[0, ...] = [-self.pix_Phantom.width() / 2, -self.pix_Phantom.height() * 3 / 4]
         self.gps_Phantom = QtWidgets.QGraphicsPixmapItem(self.pix_Phantom)
-        self.gps_Phantom.setOffset(self.offset[0, 0], self.offset[0, 1])
-        self.gps_Phantom.setPos(-  self.offset[0, 0], -self.offset[0, 1])
-        self.gps_Phantom = QtWidgets.QGraphicsPixmapItem(self.pix_Phantom)
+        #self.gps_Line = QtWidgets.QGraphicsLineItem()
         self.gs_Phantom = QtWidgets.QGraphicsScene()
         self.gs_Phantom.clear()
         self.gs_Phantom.addItem(self.gps_Phantom)
+        self.Line1 = self.gs_Phantom.addLine(0, 0, 65, 216, QtCore.Qt.white)
+        self.Line2 = self.gs_Phantom.addLine(0, 0, 245, 45, QtCore.Qt.white)
+        self.Line3 = self.gs_Phantom.addLine(65, 216, 245, 45, QtCore.Qt.white)
         self.gV_Phantom.setScene(self.gs_Phantom)
         self.gV_Phantom.setStyleSheet("background:black")
         self.gs_Phantom.update()
 
 
     #slide bar with image movement
-    #def Phantom_Slider_Moved(self):
-       # self.hSlider_Phantom.valueChanged.connect(self.hslider_changed)
+    def Phantom_Slider_Moved(self):
+        self.hSlider_Phantom.valueChanged.connect(self.hslider_changed)
 
 
-    #def hslider_changed(self):
-        #hSlider_Phantom_value = self.hSlider_Phantom.value()
+    def hslider_changed(self):
+        self.hSlider_Phantom_value = self.hSlider_Phantom.value()
+        print(self.hSlider_Phantom_value)
+        self.rotation_triangle()
 
-
+    def rotation_triangle(self):
+        self.offset = np.zeros(shape=(3, 2))
+        self.offset[0, ...] = [-self.pix_Phantom.width() / 2, -self.pix_Phantom.height()  / 2]
+        self.Line1.setPos(-self.offset[0, 0], -self.offset[0, 1])
+        self.Line2.setPos(-self.offset[0, 0], -self.offset[0, 1])
+        self.Line3.setPos(-self.offset[0, 0], -self.offset[0, 1])
+        self.Line1.setTransformOriginPoint(self.Line1.mapFromScene(QtCore.QPointF(95,95)))
+        self.Line2.setTransformOriginPoint(self.Line1.mapFromScene(QtCore.QPointF(95,95)))
+        self.Line3.setTransformOriginPoint(self.Line1.mapFromScene(QtCore.QPointF(95,95)))
+        self.Line1.setRotation(self.hSlider_Phantom_value)
+        self.Line2.setRotation(self.hSlider_Phantom_value)
+        self.Line3.setRotation(self.hSlider_Phantom_value)
 
 if __name__ == '__main__':
     import sys
@@ -71,6 +83,7 @@ if __name__ == '__main__':
     ui = fanbeam_main()
     ui.setupUi(wid_FanRecont)
     ui.PhantomSelect_click()
-    #ui.Phantom_Slider_Moved()
+    ui.Phantom_Slider_Moved()
+    #ui.rotation_triangle()
     wid_FanRecont.show()
     sys.exit(app.exec_())
