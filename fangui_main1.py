0ec1af75bfd627ec1e725c842ca1c9c467d486f0
jananiperumal <Janani@Jananis-MacBook-Pro.local>
2017-06-25 07:23:10 +0200

    commit

--- fangui_main1.py
+++ fangui_main1.py
@@ -1,13 +1,11 @@
 from __future__ import division
 from __future__ import print_function
 from PIL import Image
-from pla
 from PyQt5 import QtCore, QtGui, QtWidgets
 from fanGUI_Project import Ui_wid_FanRecont
 from PhantomSelect_Window import selectPhantom
 from PhantomSelect import Ui_Wid_PhantomSelect
 import scipy.misc
-import math
 import sys
 import subprocess
 import numpy as np
@@ -29,18 +27,109 @@ class fanbeam_main(Ui_wid_FanRecont):
         #self.setupUi(dialog)
 
 
-    def radonRayDrivenApproach(image ,detectorSize ,dSI ,detectorSpacing,angStepSize,angularRange,numDet,numProj,dDI):
-        angStepSize = angularRange / numProj
-        maxDetIndex = numDet / detectorSpacing
-        dSD = dSI + dDI
-        maxBetaIndex = angularRange / angStepSize
-        detectorSizeIndex = detectorSize / detectorSpacing
-        samplingRate = 3
-        fanogram = np.ndarray((detectorSizeIndex, maxBetaIndex))
-        fanogram = np.arange(detectorSpacing, angStepSize)
-        fanogram = np.random(-(detectorSizeIndex-1.0) * detectorSpacing/2.0,0)
-        trans = trans
+    def get_det_coords(size, spacing):
+        latmax = (size / 2 - .5) * spacing
+        lat = np.linspace(-latmax, latmax, size, endpoint=True)
+        return lat
 
+    def get_fan_coords(size, spacing, distance, numang):
+        latmax = (size / 2 - .5) * spacing
+        angmax = abs(np.arctan2(latmax, distance))
+        # equispaced angles
+        angles = np.linspace(-angmax, angmax, numang, endpoint=True)
+
+        latang = np.tan(angles) * distance
+        latang[0] = -latmax
+        latang[-1] = latmax
+        return angles, latang
+
+    def radon_fan_translation(arr, det_size, det_spacing=1, shift_size=1,
+                              lS=1, lD=None, return_ang=False,
+                              jmc=None, jmm=None):
+        N = arr.shape[0]
+        print(N)
+        if lD is None:
+            lD = N / 2
+        lS = abs(lS)
+        lDS = lS + lD
+
+        numsteps = int(np.ceil((N + det_size) / shift_size))
+        print(numsteps)
+
+        if jmm is not None:
+            jmm.value = numsteps + 2
+
+        # First, create a zero-padded version of the input image such that
+        # its center is the source - this is necessary because we can
+        # only rotate through the center of the image.
+        # arrc = np.pad(arr, ((0,0),(N+2*lS-1,0)), mode="constant")
+        if lS <= N / 2:
+            pad = 2 * lS
+            arrc = np.pad(arr, ((0, 0), (pad, 0)), mode="constant")
+            # The center is where either b/w 2 pixels or at one pixel,
+            # as it is for the input image.
+        else:
+            pad = N / 2 + lS
+            if pad % 1 == 0:  # even
+                pad = int(pad)
+                arrc = np.pad(arr, ((0, 0), (pad, 0)), mode="constant")
+            else:
+                pad = int(np.floor(pad))
+                arrc = np.pad(arr, ((0, 0), (pad, 0)), mode="constant")
+        axi = np.ones(det_size) * lDS
+
+        if det_size % 2 == 0:
+            even = True
+        else:
+            even = False
+
+        if jmc is not None:
+            jmc.value += 1
+
+        lat = fanbeam_main.get_det_coords(det_size, det_spacing)
+        print(lat)
+
+        angles = np.arctan2(lat, axi)
+
+        pad2 = det_size
+        padset = np.pad(arrc, ((0, pad2), (0, 0)), mode="constant")
+        A = angles.shape[0]
+        lino = np.zeros((numsteps, A))
+
+        if jmc is not None:
+            jmc.value += 1
+
+        for i in range(numsteps):
+            padset = np.roll(padset, shift_size, axis=0)
+            # cut out a det_size slice
+            curobj = padset[N:]
+            print(curobj)
+            for j in range(A):
+                ang = angles[j]
+                rotated = scipy.ndimage.rotate(curobj, ang / np.pi * 180,
+                                               order=3, reshape=False, mode="constant", cval=0)
+                #print(rotated)
+                if even:
+                    warnings.warn("For even images the linogram is of " +
+                                  "reduced resolution, because the center " +
+                                  "of the image does not coincide with " +
+                                  "a center of a pixel.")
+                    centerid = int(len(rotated) / 2)
+                    lino[i, j] = np.sum(rotated[centerid - 1:centerid + 1, :]) / 2
+                    #print(lino)
+                else:  # odd
+                    centerid = int(np.floor(len(rotated) / 2))
+                    lino[i, j] = np.sum(rotated[centerid, :])
+
+            if jmc is not None:
+                jmc.value += 1
+
+        if return_ang:
+            scipy.misc.imsave('outfile.jpg', lino)
+            return lino, angles
+        else:
+            scipy.misc.imsave('outfile.jpg', lino)
+            return lino
 
     #Logic for clicking the push button and getting new window
     def PhantomSelect_click(self):
