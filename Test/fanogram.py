10df7c8437041478b7614f35e1e1df74c565755f
Janani <janani.perumal85@gmail.com>
2017-09-13 22:05:39 +0200

    without artifacts

--- Test/fanogram.py
+++ Test/fanogram.py
@@ -1,14 +1,15 @@
 from __future__ import division
 from __future__ import print_function
-from Test.projector import radonRayDrivenApproach as rrd
-from Test.projector import interpolation_Image as inter
-from pyconrad import PyConrad, ImageUtil, java_float_dtype
 import numpy as np
 import math
-
-import matplotlib.pyplot as plt
-from Test.projector import plot_interp as pl
 from PIL import Image,ImageChops
+from pyconrad import *
+
+jvm = PyConrad()
+jvm.setup()
+jvm.add_import('edu.stanford.rsl.conrad.data.numeric')
+jvm.add_import('edu.stanford.rsl.tutorial.phantoms')
+jvm.add_import('edu.stanford.rsl.conrad.phantom')
 
 #Pyconrad
 pyconrad = PyConrad()
@@ -16,17 +17,32 @@ pyconrad.setup()
 pyconrad.start_conrad()
 
 ####Declaration of variables
-gammaM = 11.768288932020647*math.pi/180
-maxT = (float)(500)
+#gammaM = 11.768288932020647*2*math.pi/180
+#maxT = (float)(500)
+#deltaT = (float)(1.0)
+#focalLength = (float)((maxT/2.0-0.5)*deltaT/math.tan(gammaM))
+#maxBeta = (float)(285.95* math.pi/180)
+#deltaBeta = (float)(maxBeta / 132)
+maxT = (float)(600)
+focalLength = (float)(500)
+gammaM = math.atan((maxT/2.0-0.5 ) / focalLength)
 deltaT = (float)(1.0)
-focalLength = (float)((maxT/2.0-0.5)*deltaT/math.tan(gammaM))
-maxBeta = (float)(285.95* math.pi/180)
-deltaBeta = (float)(maxBeta / 132)
+numProj = 500
+maxBeta = math.pi +  2*gammaM;
+deltaBeta = (float)(maxBeta/numProj )
 rayfil = (int) (maxT / deltaT)
+
 
 ####Input image
 img = Image.open("phantom.png")
-Phantom = pyconrad.classes.stanford.rsl.conrad.data.numeric.Grid2D.from_numpy(np.array(img))
+Phantom = jvm['Grid2D'].from_numpy(np.array(img))
+Phantom.setOrigin(JArray(JDouble)([-(377 * Phantom.getSpacing()[0]) / 2, -(377 * Phantom.getSpacing()[1]) / 2 ]))
+Phantom.setSpacing(JArray(JDouble)([deltaT , deltaT]))
+Phantom.show()
+
+# Phantom = pyconrad.classes.stanford.rsl.conrad.data.numeric.Grid2D.from_numpy(np.array(img))
+#Phantom.setSpacing(deltaT , deltaT )
+#Phantom.setOrigin(-(377 * Phantom.getSpacing()[0]) / 2, -(377 * Phantom.getSpacing()[1]) / 2  )
 
 #####Forward Projection
 Test = pyconrad.classes.stanford.rsl.tutorial.fan.FanBeamProjector2D(focalLength, maxBeta, deltaBeta, maxT, deltaT)
@@ -41,13 +57,13 @@ weight = pyconrad.classes.stanford.rsl.tutorial.fan.redundancy.ParkerWeights(foc
 pyconrad.classes.stanford.rsl.conrad.data.numeric.NumericPointwiseOperators.multiplyBy(fanogram, weight)
 weight.show("Weight")
 
-###Ray by ray filtering
+###Ramlak filtering
 sizeimage =  fanogram.getSize()
-rbrfil = pyconrad.classes.stanford.rsl.tutorial.filters.RayByRayFiltering(rayfil , deltaT, 0.5,0.000200, 1., 1000000.0, 6, 405)
+ramlak = pyconrad.classes.stanford.rsl.tutorial.filters.RamLakKernel((int) (maxT / deltaT), deltaT)
 print(sizeimage)
 
-for theta in range(0,132):
-    rbrfil.applyToGrid(fanogram.getSubGrid(theta))
+for theta in range(0,500):
+    ramlak.applyToGrid(fanogram.getSubGrid(theta))
 fanogram.show("After filtering")
 
 #####Backward Projection
@@ -57,10 +73,3 @@ baclpro = Test1.initSinogramParams(fanogram)
 back = Test1.backprojectRayDriven(fanogram)
 back.show("back projection")
 
-
-#pl(image)
-#fanogram = rrd(img,inter(arr),500,50,20,500,1,377)
-#fanogram = rrd(arr,inter(arr),1500,100,120,600,1,500)
-#fan = rrd(inter(arr))
-#pl(img,fanogram)
-#pl(img,fan)
