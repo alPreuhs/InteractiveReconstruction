1dc0671e31df7ff02d22029424d18827e525b8fc
Janani <janani.perumal85@gmail.com>
2017-09-25 21:35:09 +0200

    latest code

--- Source/fangui_main.py
+++ Source/fangui_main.py
@@ -1,94 +1,32 @@
 from __future__ import division
 from __future__ import print_function
 from PIL import Image
-from scipy.interpolate import RectBivariateSpline
-from numpy import ones,vstack
-from numpy.linalg import lstsq
 from PyQt5 import QtCore, QtGui, QtWidgets
 from Source.fanGUI_Project import Ui_wid_FanRecont
-from numpy import linalg as LA
 from Source.PhantomSelect_Window import selectPhantom
-from sklearn import preprocessing
-from sympy import Point3D, Line3D
+from pyconrad import *
 import math
 import numpy as np
-import matplotlib.pylab as plt
+import qimage2ndarray
+import scipy.misc
 
+jvm = PyConrad()
+jvm.setup()
+jvm.add_import('edu.stanford.rsl.conrad.data.numeric')
+jvm.add_import('edu.stanford.rsl.tutorial.phantoms')
+jvm.add_import('edu.stanford.rsl.conrad.phantom')
+
+#Pyconrad
+pyconrad = PyConrad()
+pyconrad.setup()
+pyconrad.start_conrad()
 
 
 class fanbeam_main(Ui_wid_FanRecont):
 
     phantom_value = {}
     file_path = 'NULL'
-    #def __init__(self, dialog):
-     #   print("test")
-        #Ui_wid_FanRecont.__init__(self)
-        #self.setupUi(dialog)
 
-    def interpolation_Image(self,image):
-        x = np.arange(image.shape[0])
-        y = np.arange(image.shape[1])
-        z = image
-        img_interp_spline = RectBivariateSpline(x=x, y=y, z=image,bbox=[None, None, None, None],kx=3, ky=3,s=0  )
-        return img_interp_spline
-
-
-
-    def radonRayDrivenApproach(self,img_interp_spline,dSI,dDI,val,detectorSize,detectorSpacing,numProj):
-        #rrd(img_interp_spline,dSI,dDI,val,detectorSize,detectorSpacing,numProj)
-
-        fan_img = Image.new('RGB', (377, 377), "black")  # create a new black image
-        pixels = fan_img.load()
-        detectorSizeIndex = (detectorSize / detectorSpacing)
-        gammaM = math.atan((detectorSize / 2.0 - 0.5) / dSI)
-        angRange = val + 2 * gammaM
-        angStepSize = angRange / numProj
-        samplingRate = 3.0
-        maxbetaindex = angRange / angStepSize
-        # print(maxbetaindex)
-        for i in np.arange(0, 10):
-            # print(val)
-            beta = val * i
-            print(beta)
-            cosBeta = math.cos(beta)
-            sinBeta = math.sin(beta)
-            source_x = dSI * (cosBeta)
-            source_y = dSI * sinBeta
-            PP_Point_x = -detectorSize / 2 * sinBeta
-            PP_Point_y = detectorSize / 2 * (cosBeta)
-            PP = (PP_Point_x, PP_Point_y)
-            source = (source_x, source_y)
-            PP_vector = np.array(PP) * (-cosBeta) + np.array(PP) * (-sinBeta)
-            dirDetector = (PP) / np.linalg.norm(PP)
-            #    print("dirDetector   ",dirDetector)
-            for t in range(0, int(detectorSizeIndex)):
-                stepsDirection = 0.5 * detectorSpacing + t * detectorSpacing
-                #        print("stepsDirection   ",stepsDirection)
-                P = np.array(PP) + (dirDetector * stepsDirection)
-                points = (source, P)
-                distance = math.hypot(PP_Point_x - source_x, PP_Point_y - source_y)
-                #        print("distance  " , distance)
-                x_coords, y_coords = zip(*points)
-                A = vstack([x_coords, ones(len(x_coords))]).T
-                m, c = lstsq(A, y_coords)[0]
-                straightline = "{m}x + {c}".format(m=m, c=c)
-                #        print (straightline)
-                increment = 1.0 / distance * samplingRate
-                sum = 0.0
-                for Linet in np.arange(0.0, distance * samplingRate):
-                    current = np.array(source) + increment * Linet
-                    #           print("current    ", current)
-                    phantomWidth = 377
-                    phantomHeight = 377
-                    if ((phantomWidth) <= ((current.item(0)) + 1)) or ((phantomHeight) <= ((current.item(1)) + 1)) \
-                            or ((current.item(0)) < 0) or ((current.item(1)) < 0):
-                        continue
-                    # sum += img_interp_spline(current.item(0), current.item(1))
-                    sum += interpolate.interp1d(source_x + 0.5, source_y + 0.5)
-                sum /= samplingRate
-                print("sum    ", sum)
-                pixels = (i, t, sum)
-        return fan_img
 
     #Logic for clicking the push button and getting new window
     def PhantomSelect_click(self):
@@ -105,12 +43,13 @@ class fanbeam_main(Ui_wid_FanRecont):
     def getPhantom(self):
         self.file_path = self.selectPhan_creator.ListWid_SelectPhantom.currentIndex().data()
         self.Phantom_load()
+        self.PhantomFFT()
         self.selectPhan_Window.close()
 
     #Function for loading the phantom
     def Phantom_load(self):
         img_Phantom = QtGui.QImage(self.phantom_value[self.file_path])
-        img_Phantom = img_Phantom.scaled(377,377, aspectRatioMode=QtCore.Qt.KeepAspectRatio,transformMode=QtCore.Qt.SmoothTransformation)
+        img_Phantom = img_Phantom.scaled(self.gV_Phantom.size(), aspectRatioMode=QtCore.Qt.KeepAspectRatio,transformMode=QtCore.Qt.SmoothTransformation)
         self.pix_Phantom = QtGui.QPixmap(img_Phantom)
         self.gps_Phantom_placeholder = QtWidgets.QGraphicsPixmapItem(self.pix_Phantom)
         self.gps_Phantom = QtWidgets.QGraphicsPixmapItem(self.pix_Phantom)
@@ -118,23 +57,166 @@ class fanbeam_main(Ui_wid_FanRecont):
         self.gs_Phantom = QtWidgets.QGraphicsScene()
         self.gs_Phantom.clear()
         self.gs_Phantom.addItem(self.gps_Phantom)
-        self.Ecllipse = self.gs_Phantom.addEllipse(0,0,25,25,QtCore.Qt.white)
-        self.Line1 = self.gs_Phantom.addLine(20, 20, 150, 375, QtCore.Qt.white)
-        self.Line2 = self.gs_Phantom.addLine(20, 20, 375,150, QtCore.Qt.white)
-        self.Line3 = self.gs_Phantom.addLine(150, 375, 375, 150, QtCore.Qt.white)
+        #self.Ecllipse = self.gs_Phantom.addEllipse(0,0,25,25,QtCore.Qt.white)
+        #self.Line1 = self.gs_Phantom.addLine(20, 20, 150, 375, QtCore.Qt.white)
+        #self.Line2 = self.gs_Phantom.addLine(20, 20, 375,150, QtCore.Qt.white)
+        #self.Line3 = self.gs_Phantom.addLine(150, 375, 375, 150, QtCore.Qt.white)
         self.gV_Phantom.setScene(self.gs_Phantom)
         self.gV_Phantom.setStyleSheet("background:black")
         self.gs_Phantom.update()
-        self.image = np.asarray(Image.open(self.phantom_value[self.file_path]), dtype="int32")
+        self.image = Image.open(self.phantom_value[self.file_path])
+
+    ####Fourier transform of the phantom
+    def PhantomFFT(self):
+        self.PhantomFFT_image = jvm['Grid2D'].from_numpy(np.array(self.image))
+        grid2dcomplex = pyconrad.classes.stanford.rsl.conrad.data.numeric.Grid2DComplex(self.PhantomFFT_image)
+        grid2dcomplex.transformForward()
+        grid2dcomplex.fftshift()
+        grid2dcomplex.show("grid2complex display2")
+
+        #PhantomFFTarray = grid2dcomplex.as_numpy()
+        #self.gs_PhantomFFT = QtWidgets.QGraphicsScene()
+        #self.gs_PhantomFFT.addPixmap(QtGui.QPixmap.fromImage(qimage2ndarray.array2qimage(PhantomFFTarray)).scaled(self.gV_Phantom_FFT.size(),
+        #                                                                                                     aspectRatioMode=QtCore.Qt.KeepAspectRatio,
+        #                                                                                                    transformMode=QtCore.Qt.SmoothTransformation))
+        #self.gV_Phantom_FFT.setScene(self.gs_PhantomFFT)
+
+    #Xray button click for forward projection
+    def Xray_Clicked(self):
+        self.pB_Xray.clicked.connect(self.forwardProj)
 
 
+    ####forward projection
+    def forwardProj(self):
+
+        ####Declare Variables
+        self.maxT = (float)(600)
+        self.focalLength = (float)(500)
+        self.gammaM = math.atan((self.maxT / 2.0 - 0.5) / self.focalLength)
+        self.deltaT = (float)(1.0)
+        self.numProj = 500
+        self.maxBeta = math.pi + 2 * self.gammaM
+        self.deltaBeta = (float)(self.maxBeta / self.numProj)
 
 
-    #slide bar with image movement
-    def Phantom_Slider_Moved(self):
-        self.hSlider_Phantom.valueChanged.connect(self.hslider_changed)
+        ####convert the input Phantom to array and set origin and spacing
+        Phantom = jvm['Grid2D'].from_numpy(np.array(self.image))
+        self.Phantomwidth = Phantom.getWidth()
+        self.Phantomheight = Phantom.getHeight()
+        Phantom.setOrigin(JArray(JDouble)([-(self.Phantomwidth * Phantom.getSpacing()[0]) / 2, -(self.Phantomheight * Phantom.getSpacing()[1]) / 2]))
+        Phantom.setSpacing(JArray(JDouble)([self.deltaT, self.deltaT]))
+        Phantom.show("Phantom")
 
 
+        ####Forward Projection
+        ForwardProj = pyconrad.classes.stanford.rsl.tutorial.fan.FanBeamProjector2D(self.focalLength, self.maxBeta, self.deltaBeta, self.maxT,
+                                                                           self.deltaT)
+        self.fanogram = ForwardProj.projectRayDriven(Phantom)
+        self.fanogram_copy = self.fanogram
+        self.fanogram.show("Fanogram before filtering")
+
+
+        #load the fanogram image
+        fanogramarray = self.fanogram.as_numpy()
+        self.gs_fanogram = QtWidgets.QGraphicsScene()
+        self.gs_fanogram.addPixmap(QtGui.QPixmap.fromImage(qimage2ndarray.array2qimage(fanogramarray)).scaled(self.gV_Sinogram.size(),aspectRatioMode=QtCore.Qt.KeepAspectRatio,transformMode=QtCore.Qt.SmoothTransformation))
+        self.gV_Sinogram.setScene(self.gs_fanogram)
+        self.gV_Sinogram.setStyleSheet("background:black")
+        self.gs_fanogram.update()
+
+
+    def Parkerweight_Check(self):
+        self.checkBox_ParkerWeigh.stateChanged.connect(self.weightcheck)
+
+    def weightcheck(self):
+        if self.checkBox_ParkerWeigh.isChecked():
+            self.parkerweight()
+        else:
+            self.fanogram = self.fanogram_copy
+
+            self.fanogram.show()
+            print("inside parker")
+
+    def parkerweight(self):
+        weight = pyconrad.classes.stanford.rsl.conrad.data.numeric.Grid2D(self.Phantomwidth, self.Phantomheight)
+        weight = pyconrad.classes.stanford.rsl.tutorial.fan.redundancy.ParkerWeights(self.focalLength, self.maxT, self.deltaT, self.maxBeta,self.deltaBeta)
+        pyconrad.classes.stanford.rsl.conrad.data.numeric.NumericPointwiseOperators.multiplyBy(self.fanogram, weight)
+        weight.show()
+
+    def Ramlak_Check(self):
+        self.checkBox_RamLakFilter.stateChanged.connect(self.filtercheck)
+
+    def filtercheck(self):
+        if self.checkBox_RamLakFilter.isChecked():
+            self.ramlakfilter()
+        else:
+            self.fanogram = self.fanogram_copy
+            print("inside ramlak")
+
+    def ramlakfilter(self):
+        sizeimage = self.fanogram.getSize()
+        ramlak = pyconrad.classes.stanford.rsl.tutorial.filters.RamLakKernel((int)(self.maxT / self.deltaT), self.deltaT)
+
+        for theta in range(0, 500):
+            ramlak.applyToGrid(self.fanogram.getSubGrid(theta))
+        self.fanogram.show("After filtering")
+
+    ####Clicking the reconstruction button
+    def Reconst_Clicked(self):
+        self.pB_Reconstruction.clicked.connect(self.BackProj)
+
+
+    ###Backprojection
+    def BackProj(self):
+
+        Backprojection = pyconrad.classes.stanford.rsl.tutorial.fan.FanBeamBackprojector2D(self.focalLength, self.deltaT, self.deltaBeta, self.Phantomwidth,
+                                                                                  self.Phantomheight)
+        #baclpro = Test1.initSinogramParams(fanogram)
+        self.back = Backprojection.backprojectRayDriven(self.fanogram)
+        self.back.show("back projection")
+
+
+        ######Display the backprojected image
+        backarray = self.back.as_numpy()
+        self.gs_backproj = QtWidgets.QGraphicsScene()
+        self.gs_backproj.addPixmap(QtGui.QPixmap.fromImage(qimage2ndarray.array2qimage(backarray)).scaled(self.gV_Backproj.size(),aspectRatioMode=QtCore.Qt.KeepAspectRatio,transformMode=QtCore.Qt.SmoothTransformation))
+        self.gV_Backproj.setScene(self.gs_backproj)
+        self.gV_Backproj.setStyleSheet("background:black")
+        self.gs_backproj.update()
+
+        self.backFFT()
+
+    ####Fourier transform of the phantom
+    def backFFT(self):
+        grid2dcomplex = pyconrad.classes.stanford.rsl.conrad.data.numeric.Grid2DComplex(self.back)
+        grid2dcomplex.transformForward()
+        grid2dcomplex.fftshift()
+        grid2dcomplex.show("grid2complex display")
+
+        #PhantomFFTarray = grid2dcomplex.as_numpy()
+        #self.gs_PhantomFFT = QtWidgets.QGraphicsScene()
+        #self.gs_PhantomFFT.addPixmap(
+         #   QtGui.QPixmap.fromImage(qimage2ndarray.array2qimage(PhantomFFTarray)).scaled(self.gV_Phantom_FFT.size(),
+          #                                                                               aspectRatioMode=QtCore.Qt.KeepAspectRatio,
+           #                                                                              transformMode=QtCore.Qt.SmoothTransformation))
+        #self.gV_Phantom_FFT.setScene(self.gs_PhantomFFT)
+
+if __name__ == '__main__':
+    import sys
+
+    app = QtWidgets.QApplication(sys.argv)
+    wid_FanRecont = QtWidgets.QWidget()
+    ui = fanbeam_main()
+    ui.setupUi(wid_FanRecont)
+    ui.PhantomSelect_click()
+    ui.Xray_Clicked()
+    ui.Reconst_Clicked()
+    ui.Parkerweight_Check()
+    ui.Ramlak_Check()
+    wid_FanRecont.show()
+    sys.exit(app.exec_())
+
+'''
     def hslider_changed(self):
         self.hSlider_Phantom_value = self.hSlider_Phantom.value()
         self.rotation_triangle(self.hSlider_Phantom_value)
@@ -148,26 +230,9 @@ class fanbeam_main(Ui_wid_FanRecont):
         self.Line2.setRotation(val)
         self.Line3.setRotation(val)
         self.Ecllipse.setRotation(val)
-        fanbeam_main.radonRayDrivenApproach(self,fanbeam_main.interpolation_Image(self,self.image),231.2,105,val,318,1,377)
+        fanbeam_main.forwardProj(self)
 
-
-
-
-
-if __name__ == '__main__':
-    import sys
-
-    app = QtWidgets.QApplication(sys.argv)
-    wid_FanRecont = QtWidgets.QWidget()
-    ui = fanbeam_main()
-    ui.setupUi(wid_FanRecont)
-    ui.PhantomSelect_click()
-    ui.Phantom_Slider_Moved()
-    wid_FanRecont.show()
-    sys.exit(app.exec_())
-
-
-
+  '''
 
 
 
